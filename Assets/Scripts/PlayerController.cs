be06502d86d11048ef134e13b3a075ec4210bf9c
Oleg20111 <lusenkooleh1@gmail.com>
2022-03-12 17:55:17 +0300

    Complete Lesson 4.3 - PowerUp and CountDown
    Add collision and trigger

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,9 @@ public class PlayerController : MonoBehaviour
     private Rigidbody playerRb;
     public float speed = 5.0f;
     private GameObject focalPoint;
+    public bool hasPowerup;
+    public float powerStrength = 15;
+    public GameObject powerupIndicator;
 
     void Start()
     {
@@ -18,5 +21,36 @@ public class PlayerController : MonoBehaviour
     {
         float forwardInput = Input.GetAxis("Vertical");
         playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
+        powerupIndicator.transform.position = transform.position;
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if(collider.CompareTag("Powerup"))
+        {
+            hasPowerup = true;
+            Destroy(collider.gameObject);
+            powerupIndicator.gameObject.SetActive(true);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Enemy") && hasPowerup)
+        {
+            Rigidbody enemyRb = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = (collision.gameObject.transform.position - transform.position);
+
+            Debug.Log(collision.gameObject.transform.position + " - " + transform.position + " - " + awayFromPlayer);
+            enemyRb.AddForce(awayFromPlayer * powerStrength, ForceMode.Impulse);
+            StartCoroutine(PowerCountdownRoutine());
+        }
+    }
+
+    IEnumerator PowerCountdownRoutine()
+    {
+        yield return new WaitForSeconds(7);
+        hasPowerup = false;
+        powerupIndicator.gameObject.SetActive(false);
     }
 }
